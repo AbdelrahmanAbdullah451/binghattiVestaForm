2c3b65441893cd47546ddd492f3ad2994b79f857
Abdelrahman Tolba <abdelrahmantolba451@gmail.com>
2025-07-03 14:57:30 +0400

    styling

--- app.js
+++ app.js
@@ -31,3 +31,11 @@ window.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+
+
+document.querySelector('#contact-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    e.target.elements.name.value = '';
+    e.target.elements.email.value = '';
+    e.target.elements.message.value = '';
+  });
